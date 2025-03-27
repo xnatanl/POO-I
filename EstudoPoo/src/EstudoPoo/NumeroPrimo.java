c1535b98b40701f9d40924d35d7d6ab586bbdc2f
Natan H. Pereira <xxnatanll@hotmail.com>
2025-03-27 20:43:52 -0300

    Add files via upload

---
+++ EstudoPoo/src/EstudoPoo/NumeroPrimo.java
@@ -0,0 +1,34 @@
+package EstudoPoo;
+
+/*
+Escreva um programa que leia um número inteiro positivo e verifique se ele é 
+um número primo. Um número primo é aquele que é divisível apenas por 1 e por ele mesmo.
+*/
+public class NumeroPrimo 
+{
+    //essa classe recebe um número inteiro;
+    private int numeroInteiro;
+    
+    // implemente getters e setters;
+    
+    public int getNumeroInteiro() {
+        return numeroInteiro;
+    }
+
+    public void setNumeroInteiro(int numeroInteiro) {
+        this.numeroInteiro = numeroInteiro;
+    }
+    
+    //implemente o método que calcule o pedido no enunciado;
+    
+    public int NumeroPrimo(){
+        int res = 1;
+        //terminar isso aq
+        if(numeroInteiro > 1 && numeroInteiro %2 != 0){
+            
+        }
+        return res;
+    }
+    
+    //  valide no setter do atributo se ele é maior que zero
+}
